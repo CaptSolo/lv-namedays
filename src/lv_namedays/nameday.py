11f9ed151740d6cecbb83bb380fa110c4a5ed473
Uldis Bojars <captsolo@gmail.com>
2025-01-27 18:56:51 +0200

    Refactoring

--- src/lv_namedays/nameday.py
+++ src/lv_namedays/nameday.py
@@ -10,7 +10,7 @@ from importlib.resources import files
 
 
 NAMEDAY_LIST = "tradic_vardadienu_saraksts.json"
-
+NAMEDAY_LIST_EXTENDED = "paplasinatais_saraksts.json"
 
 def read_namedays():
     """Read the name day data from the JSON file."""
@@ -21,6 +21,25 @@ def read_namedays():
         namedays = json.load(f)
 
     return namedays
+
+
+class NameDayDB:
+    def __init__(self):
+        self.namedays = read_namedays()
+
+    def get_names_for_date(self, date:str) -> list | None:
+        return self.namedays.get(date, None)
+
+    def get_date_for_name(self, name:str) -> str | None:
+        # Make search case insensitive
+        namedays = {date: [n.lower() for n in names] for date, names in self.namedays.items()}
+
+        # Search for the name in the calendar
+        for date, names in namedays.items():
+            if name.lower() in names:
+                return date
+        return None
+
 
 def get_date_for_name(name):
 
